0ebf9fc92f3232ba92968cda6581c1167d40dcb4
ci010 <cijhn@hotmail.com>
2016-11-27 18:26:58 -0800

    template bugs;

--- src/mis-instructions/instruction_impl.cpp
+++ src/mis-instructions/instruction_impl.cpp
@@ -7,14 +7,16 @@
 namespace mis {
 
     template<typename T>
-    T Getter::get(VirtualMachine::Runtime &runtime, std::vector<mis::VirtualMachine::Work *>::iterator &flow) {
+    T Getter<T>::get(VirtualMachine::Runtime &runtime, std::vector<mis::VirtualMachine::Work *>::iterator &flow) {
         return func(runtime, tk);
     }
 
-    Getter::Getter(const Parser::Token &tk, const std::function<T(VirtualMachine::Runtime &, Parser::Token &)> &func) :
+    template<typename T>
+    Getter<T>::Getter(const Parser::Token &tk, const std::function<T(VirtualMachine::Runtime &, Parser::Token &)> &func) :
             tk(tk), func(func) {}
 
-    Getter::Getter(const Getter &o) : tk(o.tk), func(o.func) {}
+    template<typename T>
+    Getter<T>::Getter(const Getter &o) : tk(o.tk), func(o.func) {}
 
 
     Getter<mis::Number> wrapRef(mis::Parser::Token
